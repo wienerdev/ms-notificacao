3ee24a24198f6dd7d5e1d642a878369e2549b00d
wienerdev <matheuswiener9@gmail.com>
2023-01-23 20:22:22 -0300

    first commit

---
+++ src/main/java/br/com/eldorado/ms/notificacao/MsNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.eldorado.ms.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eldorado/ms/notificacao/MsNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eldorado.ms.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
